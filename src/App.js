2c7243d14e1ba4be623da32c97cd37fc41afb0ac
Taiki Noda <taiki.nd.real@gmail.com>
2022-02-04 18:00:02 +0900

    ルーティング

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-function App() {
+import React, { Component, Fragment } from 'react'; 
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+export default function App() {
+
+
+const Home = () => {
+  return <h2>HOME</h2>
+}
+
+const Movies = () => {
+  return <h2>Movies</h2>
+}
+
+const Admin = () => {
+  return <h2>MANAGE CATALOGUE</h2>
+}
+
   return (
-    <>
+    <Router>
       <div className="container">
         <div className="row">
           <h1 className="mt-3">
@@ -14,26 +31,34 @@ function App() {
             <nav>
               <ul className="list-group">
                 <li className="list-group-item">
-                  <a href="/">HOME</a>
+                  <Link to="/">HOME</Link>
                 </li>
                 <li className="list-group-item">
-                  <a href="/">Movies</a>
+                  <Link to="/movies">MOVIES</Link>
                 </li>
                 <li className="list-group-item">
-                  <a href="/">Manage Catalogue</a>
+                  <Link to="/admin">MANAGE CATALOGUE</Link>
                 </li>
               </ul>
             </nav>
           </div>
 
           <div className="col-md-2">
-
+            <Switch>
+              <Route path="/movies">
+                <Movies />
+              </Route>
+              <Route path="/admin">
+                <Admin  />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
           </div>
           
         </div>
       </div>
-    </>
-  )
+    </Router>
+  );
 }
-
-export default App;
