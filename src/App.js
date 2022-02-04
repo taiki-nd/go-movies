7f4436f588559fd2ee7bf4a0bd350a0ab17eaaf7
Taiki Noda <taiki.nd.real@gmail.com>
2022-02-04 22:08:20 +0900

    詳細ページへのルーティング

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'; 
-import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, useParams } from 'react-router-dom';
 import { Home } from './components/Home'
-import { Movies } from './components/Movies'
+import Movies from './components/Movies'
 import { Admin } from './components/Admin'
 
 export default function App() {
@@ -35,6 +35,9 @@ export default function App() {
 
           <div className="col-md-10">
             <Switch>
+              <Route path="/movies/:id">
+                <Movie />
+              </Route>
               <Route path="/movies">
                 <Movies />
               </Route>
@@ -51,4 +54,9 @@ export default function App() {
       </div>
     </Router>
   );
+}
+
+function Movie() {
+  let { id } = useParams();
+  return <h2>Movie id {id}</h2>
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-export const Movies = () => {
-  return(
-    <>
-      <h2>choose a movie! </h2>
-    </>
-  )
-};
+export default class Movies extends Component {
+
+  state = { movies: [] };
+
+  componentDidMount() {
+    this.setState({
+      movies:[
+        {id: 1, title: "load of the ring 1", runtime: 154},
+        {id: 2, title: "load of the ring 2", runtime: 149},
+        {id: 3, title: "load of the ring 3", runtime: 161}
+      ]
+    })
+  };
+
+  render() {
+    return(
+      <>
+        <h2>choose a movie! </h2>
+
+        <ul>
+          {this.state.movies.map((m) => (
+            <li key={m.id}>
+              <Link to={`movies/${m.id}`}>{m.title}</Link>              
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }  
+}
