14837a7e567cbd0a13688eb495d5b69294978114
Taiki Noda <taiki.nd.real@gmail.com>
2022-02-16 18:59:31 +0900

    エラー対応

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -6,21 +6,42 @@ export default class Movies extends Component {
   state = {
     movies: [],
     isLoaded: false,
+    error: null,
   };
 
   componentDidMount() {
-    fetch("http://localhost:4000/v1/movies")
-    .then(response => response.json())
+    fetch("http://localhost:4000/v1/moviesxx")
+    //.then(response => response.json())
+    .then(response => {
+      console.log("status code is", response.status)
+      if (response.status != 200){
+        let err = Error;
+        err.message = "Invalid status code: " + response.status;
+        this.setState({error: err});
+      }
+      return response.json()
+    })
     .then(json => {
       this.setState({
         movies: json.movies,
         isLoaded: true,
-      })
+      },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      )
     })
   };
 
   render() {
-    const { movies, isLoaded } = this.state
+    const { movies, isLoaded, error } = this.state
+
+    if (error) {
+      return <div>error: {error.message}</div>
+    }
 
     if(!isLoaded){
       return <p>Loading...</p>
