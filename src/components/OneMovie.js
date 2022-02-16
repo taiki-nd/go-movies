bab5dab02a6b1be78d42c19dd4731ed98e36ad30
Taiki Noda <taiki.nd.real@gmail.com>
2022-02-16 19:56:13 +0900

    映画詳細ページの作成

--- src/components/OneMovie.js
+++ src/components/OneMovie.js
@@ -2,37 +2,89 @@ import React, { Component } from 'react';
 
 export default class OneMovie extends Component{
 
-  state = { movie: {} }
+  state = { movie: {}, isLoaded: false, error: null }
 
   componentDidMount(){
-    this.setState({
-      movie: {
-        id: this.props.match.params.id,
-        title: "Some movie",
-        runtime: 150,
+    fetch("http://localhost:4000/v1/movies/" + this.props.match.params.id)
+    //.then(response => response.json())
+    .then(response => {
+      console.log("status code is", response.status)
+      if (response.status != 200){
+        let err = Error;
+        err.message = "Invalid status code: " + response.status;
+        this.setState({error: err});
       }
+      return response.json()
+    })
+    .then(json => {
+      this.setState({
+        movie: json.movie,
+        isLoaded: true,
+      },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      )
     })
   }
 
   render(){
-    return(
-      <>
-        <h2>Movie: {this.state.movie.title} </h2> 
-        <table className="table table-compact table-striped">
-          <thead></thead>
-          <tbody>
-            <tr>
-              <td><strong>Title:</strong></td>
-              <td>{this.state.movie.title}</td>
-            </tr>
-            <tr>
-              <td><strong>Run Time:</strong></td>
-              <td>{this.state.movie.runtime} minutes </td>
-            </tr>
-          </tbody>
-        </table>
-      </>
-    );
+    const { movie, isLoaded, error } = this.state;
+
+    if (movie.genres){
+      movie.genres = Object.values(movie.genres);
+    }else{
+      movie.genres = [];
+    }
+
+    if (error) {
+      return <div>error: {error.message}</div>
+    }
+
+    if(!isLoaded){
+      return <p>Loading...</p>
+    }else{
+      return(
+        <>
+          <h2>Movie: {movie.title} ({movie.year}) </h2> 
+
+          <div className="float-start">
+            <small>rating: {movie.mpaa_rating}</small>
+          </div>
+          <div className="float-end">
+            {movie.genres.map((m, index) => (
+              <span className="badge bg-secondary me-1" key={index}>
+                {m}
+              </span>
+            ))}
+          </div>
+
+          <div className="clearfix"></div>
+          <hr />
+
+          <table className="table table-compact table-striped">
+            <thead></thead>
+            <tbody>
+              <tr>
+                <td><strong>Title:</strong></td>
+                <td>{movie.title}</td>
+              </tr>
+              <tr>
+                <td><strong>Description:</strong></td>
+                <td>{movie.description}</td>
+              </tr>
+              <tr>
+                <td><strong>Run Time:</strong></td>
+                <td>{movie.runtime} minutes </td>
+              </tr>
+            </tbody>
+          </table>
+        </>
+      );
+    }
   }
 
 }
